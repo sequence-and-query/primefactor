054f609ea4927f202ecb10936f53070f6d19f184
sh <lifeisvector@gmail.com>
2025-06-04 10:11:14 +0900

    [FEATURE] 11p

--- src/test/java/PrimeFactorTest.java
+++ src/test/java/PrimeFactorTest.java
@@ -1,5 +1,12 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrimeFactorTest {
+    @Test
+    public void testPrimeFactorOf1() {
+        PrimeFactor primeFactor = new PrimeFactor();
 
+        assertEquals(10, 10);
+    }
 }
