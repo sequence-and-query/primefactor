c41a401840fef9bffc82aab2bec2e8d7fe750b83
sh <lifeisvector@gmail.com>
2025-06-04 10:25:28 +0900

    [REFACTORING] 37p

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -6,11 +6,8 @@ public class PrimeFactor {
     public Object of(int number) {
         List<Integer> factors = new ArrayList<>();
 
-        if(number == 2) {
-            factors.add(2);
-        }
-        else if(number == 3) {
-            factors.add(3);
+        if(number > 1) {
+            factors.add(number);
         }
 
         return factors;
