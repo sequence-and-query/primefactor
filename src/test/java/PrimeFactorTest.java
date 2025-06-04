3a9d10e7af3d7468b29f4db8d86ae4bf8a15054d
sh <lifeisvector@gmail.com>
2025-06-04 10:41:31 +0900

    [FEATURE] 53p

--- src/test/java/PrimeFactorTest.java
+++ src/test/java/PrimeFactorTest.java
@@ -43,4 +43,9 @@ class PrimeFactorTest {
     public void testPrimeFactorOf9() {
         assertEquals(Arrays.asList(3, 3), primeFactor.of(9));
     }
+
+    @Test
+    public void testPrimeFactorOf12() {
+        assertEquals(Arrays.asList(2, 2, 3), primeFactor.of(12));
+    }
 }
